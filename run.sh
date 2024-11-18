1f00da50133719909946a770ad638318edffcc35
Simon Willison <swillison@gmail.com>
2024-11-17 22:29:29 -0800

    Added sqlite-utils, datasette, shot-scraper, s3-credentials

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./build-docs.sh https://github.com/simonw/llm llm
+./build-docs.sh https://github.com/simonw/datasette datasette
+./build-docs.sh https://github.com/simonw/sqlite-utils sqlite-utils
+./build-docs.sh https://github.com/simonw/shot-scraper shot-scraper
+./build-docs.sh https://github.com/simonw/s3-credentials s3-credentials
