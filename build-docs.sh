ae3108a012b2987f6fbdfd61e1a20a05ac5697d2
Simon Willison <swillison@gmail.com>
2024-11-17 22:13:16 -0800

    Renamed version-docs to llm

--- build-docs.sh
+++ build-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION_DOCS="$PWD/version-docs"
-mkdir -p "$VERSION_DOCS"
+LLM_DOCS="$PWD/llm"
+mkdir -p "$LLM_DOCS"
 
 rm -rf /tmp/llm
 git clone https://github.com/simonw/llm /tmp/llm
@@ -9,7 +9,7 @@ cd /tmp/llm || exit
 # Loop through each tag in the repository
 for TAG in $(git tag); do
     # Check if TAG.txt already exists, and skip if it does
-    if [ -f "$VERSION_DOCS/$TAG.txt" ]; then
+    if [ -f "$LLM_DOCS/$TAG.txt" ]; then
         echo "Skipping $TAG, $TAG.txt already exists."
         continue
     fi
@@ -22,6 +22,6 @@ for TAG in $(git tag); do
     # Check if any docs/*.md files exist
     if ls docs/*.md &>/dev/null; then
         # Run the command and output to TAG.txt
-        files-to-prompt docs/*.md -c > "$VERSION_DOCS/$TAG.txt"
+        files-to-prompt docs/*.md -c > "$LLM_DOCS/$TAG.txt"
     fi
 done
